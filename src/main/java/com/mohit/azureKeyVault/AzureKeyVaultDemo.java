cf5f345a6259ada01400368626059dda3ab1147f
Mohit Gupta <mohit.mail421@gmail.com>
2019-01-29 18:37:37 +0530

    Adding support to extract required Service Principle(for accessing KeyVault) from jceks binary encrypted file(generated using hadoops credential api/cli)

--- src/main/java/com/mohit/azureKeyVault/AzureKeyVaultDemo.java
+++ src/main/java/com/mohit/azureKeyVault/AzureKeyVaultDemo.java
@@ -1,44 +1,74 @@
 package com.mohit.azureKeyVault;
 
-import com.microsoft.aad.adal4j.AuthenticationContext;
-import com.microsoft.aad.adal4j.AuthenticationResult;
-import com.microsoft.aad.adal4j.ClientCredential;
-import com.microsoft.azure.keyvault.authentication.KeyVaultCredentials;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URISyntaxException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import com.microsoft.azure.keyvault.KeyVaultClient;
-//import com.microsoft.azure.keyvault.KeyVaultClientService;
-//import com.microsoft.azure.keyvault.KeyVaultConfiguration;
 import com.microsoft.azure.keyvault.authentication.KeyVaultCredentials;
-import com.microsoft.azure.keyvault.models.KeyOperationResult;
-import com.microsoft.azure.keyvault.webkey.JsonWebKeyEncryptionAlgorithm;
 
 import com.microsoft.azure.keyvault.models.SecretBundle;
 
+//for jceks
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.ArrayList;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.security.alias.CredentialProvider;
+import org.apache.hadoop.security.alias.CredentialProviderFactory;
+import java.io.IOException;
+
 public class AzureKeyVaultDemo {
 
-	public static void main(String[] args)
-			throws InterruptedException, ExecutionException, URISyntaxException, UnsupportedEncodingException {
-		
-    String clientId = args[0];  
-    String clientKey = args[1];  
-    String vaultURI = args[2]; 
+  public static void main(String[] args) {
 
-    // ClientSecretKeyVaultCredential is the implementation of KeyVaultCredentials
+    System.out.println("args : " + args[0] + "\n" + args[1] + "\n" + args[2] + "\n");
+    String clientId = "";//args[0]; 
+    String clientKey = "";//args[1];  
+    String vaultURI = args[2];
+
+    HashMap<String, String> keymap = new HashMap<String, String>();
+    try {
+      keymap = getKeysFromJcek("clientId,clientCredential");
+      System.out.println("jcek keyPasses = " + keymap);
+    } catch (Exception e) {
+      System.out.println("jcek Exception : " + e);
+    }
+
+    clientId = keymap.get("clientId");
+    clientKey = keymap.get("clientCredential");
+
+    /* Get keyVault client by providing authorized credentials as read from jceks */
     KeyVaultClient client = new KeyVaultClient(new ClientSecretKeyVaultCredential(clientId, clientKey));
-		System.out.println(client);
+    //System.out.println(client);
+    
+    //Keys to be extracted from KeyVault
+    ArrayList<String> keys = new ArrayList<String>();
+    keys.add("endpointUrl");
+    keys.add("clientId");
+    keys.add("clientCredential");
+    for (String key : keys) {
+      SecretBundle secret = client.getSecret(vaultURI, key);
+      System.out.println("===> " + key + " : " + secret.value());
+    }
+  }
 
-		System.out.println("calling client.getSecret");
-    SecretBundle secret = client.getSecret(vaultURI, "clientId" );
-		System.out.println("after client.getSecret");
-    System.out.println(secret.value());
-	}
+  public static HashMap<String, String> getKeysFromJcek(String csvKeys) throws IOException {
+
+    HashMap<String, String> keyPasses = new HashMap<String, String>();
+    String[] keys = csvKeys.split(",");
+    System.out.println(Arrays.toString(keys));
+
+    System.out.println("Fetch password from configured credential provider path");
+    Configuration c = new Configuration();
+    c.set(CredentialProviderFactory.CREDENTIAL_PROVIDER_PATH, "localjceks://file/Users/mohit.gupta/Downloads/azure.jceks");
+    CredentialProvider credentialProvider = CredentialProviderFactory.getProviders(c).get(0);
+    for(String key : keys) {
+      CredentialProvider.CredentialEntry entry = credentialProvider.getCredentialEntry(key);
+      if (entry == null) {
+        throw new IOException(String.format("No credential entry found for %s", key));
+      } else {
+        keyPasses.put(key, String.valueOf(entry.getCredential()));
+        //System.out.println(String.valueOf(entry.getCredential());
+      }
+    }
+    return keyPasses;
+  }
 }
 
